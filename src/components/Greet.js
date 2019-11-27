c65138767e1b0ef53bea6e2d2ca4501c0eb77692
whoseRon <whose69@gmail.com>
2019-11-28 01:21:53 +0200

    rewrite Greet in ES6 syntax

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
-export default function Greet() {
-    return <h1>Hi Oz</h1>
-}
+// export default function Greet() {
+//     return <h1>Hi Oz</h1>
+// }
+
+const Greet = () => <h1>Hi Oz</h1>;
+
+export default Greet;
